b8df05d6b835774fc22e24d5f310d9e76705da3f
mcYiu <mcyiu@tlu.ee>
2023-05-17 12:14:31 +0300

    debug.UIsound

--- Assets/UISound.cs
+++ Assets/UISound.cs
@@ -3,6 +3,25 @@ using UnityEngine;
 public class UISound : MonoBehaviour
 {
     public AudioSource btnSound;
+
+    private static UISound instance = null;
+    public static UISound Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(transform.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(transform.gameObject);
+    }
     public void BtnSound()
     {
         btnSound.Play();
